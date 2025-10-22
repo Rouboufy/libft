93fad0a9a9792b28736385effdadc3ea1e1c4a35
Rouboufy <213021409+Rouboufy@users.noreply.github.com>
2025-10-22 21:37:20 +0900

    atoi

--- ft_atoi.c
+++ ft_atoi.c
@@ -1,39 +1,15 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strnstr.c                                       :+:      :+:    :+:   */
+/*   ft_atoi.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: blanglai <blanglai@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/10/22 18:37:46 by blanglai          #+#    #+#             */
-/*   Updated: 2025/10/22 21:36:03 by blanglai         ###   ########.fr       */
+/*   Created: 2025/10/22 21:06:24 by blanglai          #+#    #+#             */
+/*   Updated: 2025/10/22 21:09:19 by blanglai         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char *ft_strnstr(const char *big, const char *small, size_t len)
-{
-	const char	*b;
-	const char	*s;
-	size_t		l;
 
-	if(*small == '\0')
-		return ((char *) big);
-	while (len > 0 && *big != '\0') {
-		b = big;
-		s = small;
-		l = len;
-		while (*b != '\0' && *s != '\0' && *b == *s && l >0)
-		{
-			b++;
-			s++;
-			l--;
-		}
-		if (*s == '\0')
-			return ((char *)big);
-		big++;
-		len--;
-	}
-	return (NULL);
-}
