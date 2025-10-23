6ae8d91e2d1d8d48861736e4e508fe8b33bc0ba8
Rouboufy <213021409+Rouboufy@users.noreply.github.com>
2025-10-23 19:04:37 +0900

    atoi unfinished

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,7 +6,7 @@
 /*   By: blanglai <blanglai@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/22 21:06:24 by blanglai          #+#    #+#             */
-/*   Updated: 2025/10/23 18:05:44 by blanglai         ###   ########.fr       */
+/*   Updated: 2025/10/23 19:03:51 by roubs            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,9 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+
+static int issign(char *nptr);
+
 int ft_atoi(const char *nptr) 
 {
 	int	res;
@@ -26,21 +29,30 @@ int ft_atoi(const char *nptr)
 	while (nptr[i] == ' ' || (nptr[i] >= 9 && nptr[i] <= 13))
 		i++;
 	if (nptr[i] == '-' || nptr[i] == '+')
-	{
-		if(nptr[i] == '-')
-		{
-			sign = -1;
-			i++;
-		}
-		if(nptr[i] == '+')
-			i++;
-	}
+		sign = issign(nptr);
 	while (nptr[i] >= '0' && nptr[i] <= '9')
 	{
 		res = res * 10 + (nptr[i] -'0');
 		i++;
 	}
 	return (res * sign);
+}
+
+static int issign(char *nptr)
+{
+	int sign;
+	int i;
+
+	i = 0;
+	sign = 1;
+	if(nptr[i] == '-')
+	{
+		sign = -1;
+		i++;
+	}
+	if(nptr[i] == '+')
+		i++;
+	return (sign);
 }
 
 int main (){
