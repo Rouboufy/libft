a99b972c6bebb737493b9152cb13809215229553
Rouboufy <213021409+Rouboufy@users.noreply.github.com>
2025-10-27 22:39:51 +0900

    norm

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,7 +6,7 @@
 /*   By: blanglai <blanglai@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/24 19:05:25 by blanglai          #+#    #+#             */
-/*   Updated: 2025/10/27 21:53:56 by blanglai         ###   ########.fr       */
+/*   Updated: 2025/10/27 22:32:51 by blanglai         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,6 @@ char	*ft_strtrim(char const *s1, char const *set)
 {
 	int	start;
 	int	end;
-	int	len;
 
 	if (!s1 || !set)
 		return (NULL);
